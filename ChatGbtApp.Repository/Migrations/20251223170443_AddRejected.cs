f976e8d7c819f416a4cef2503d06d615707efb30
Michal Jankowski <mjankowski3000@gmail.com>
2025-12-24 12:15:58 +0100

    aa

---
+++ ChatGbtApp.Repository/Migrations/20251223170443_AddRejected.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatGbtApp.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRejected : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Rejected",
+                table: "Jobs",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rejected",
+                table: "Jobs");
+        }
+    }
+}
